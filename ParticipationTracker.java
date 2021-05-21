e1c5339717e0171c88e636d54610dfe51f33fbf8
Adarsh Padalia <340943950@gapps.yrds.ca>
2021-05-21 08:29:49 -0400

    Added method that finds file path through a file explorer pop-up

--- ParticipationTracker.java
+++ ParticipationTracker.java
@@ -4,7 +4,49 @@
  * Teacher: Mr. Ho
  * Description: Tracking participation in a classroom setting
  * */
- 
+
+import java.io.IOException;
+import javax.swing.*;
+import javax.swing.filechooser.*;
+
  public class ParticipationTracker {
-    
+    public static void main(String[] args) throws IOException {
+        
+    }
+
+    /**
+     * This method can open a file explorer pop-up that finds the path of a selected file (returns an 
+     * empty string if the user didn't select a file)
+     * 
+     * The filetype can also be restricted so that only files of that file type show up (the file 
+     * description is not used if fileType is left blank)
+     * 
+     * @param fileDescription   Description of the restricted file type (e.g. "CSV Files")
+     * @param fileType          Restrict the filetypes (csv, txt, etc.) shown in the file explorer pop-up
+     * @return fileName         The path of the file that the user selected
+     */
+    public static String getFilePath(String fileDescription, String fileType) {
+        if (!fileDescription.equals("")) {
+            fileDescription = fileType;
+        }
+        
+        JFileChooser fc = new JFileChooser();
+        if (!fileType.equals("")) {
+            FileFilter filter = new FileNameExtensionFilter(fileDescription, fileType);
+            fc.setFileFilter(filter);
+        }
+        int response = fc.showOpenDialog(null);
+
+        String filePath;
+        if (response == JFileChooser.APPROVE_OPTION) {
+            filePath = fc.getSelectedFile().toString();
+        }
+        else if (response == JFileChooser.ERROR_OPTION) {
+            filePath = "";
+        }
+        else {
+            filePath = "";
+        }
+        return filePath;
+    }
  }
